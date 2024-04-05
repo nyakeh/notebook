c7276f7060af48dcd1c98d97456f621a6581d7ab
Nyakeh Rogers <nyakeh@users.noreply.github.com>
2024-04-05 16:11:24 +0100

    Added chapter describing the syntax of passing props to a component.

--- react-ts-guide/src/App.tsx
+++ react-ts-guide/src/App.tsx
@@ -15,10 +15,27 @@ function App() {
 			<Content
 				title="Testing"
 				body="You can write unit tests using the Jest NPM library. Jest is a popular JavaScript testing framework that's commonly used for testing React applications. Using the beforeAll syntax 'beforeAll(() => {});' as a shared test setup function."
-				codeSnippets={["describe('Given a unit test method', () => {","it('when in a test case', () => { const result = plus(4, 3); expect(result).toEqual(7); });","it('when a second case', () => { const result = plus(12, 4); expect(result).toEqual(16); });","});"]}
+				codeSnippets={[
+					"describe('Given a unit test method', () => {",
+					"it('when in a test case', () => { const result = plus(4, 3); expect(result).toEqual(7); });",
+					"it('when a second case', () => { const result = plus(12, 4); expect(result).toEqual(16); });",
+					"});",
+				]}
 				links={[{ label: "Jest docs", url: "https://jestjs.io/docs/setup-teardown" }]}
 			/>
-			<Content title="Third" body="" codeSnippets={[]} links={[{ label: "", url: "" }]} />
+			<Content
+				title="Function Props"
+				body="Passing props to components allows you to create reusable and dynamic UI components in React. The code snippet defines an input type and component constructor that receives the parameters."
+				codeSnippets={[
+					"type PlusParametersDto = {     first: number,     second: number }",
+					"function plus({first, second}: PlusParametersDto): number {     return { first + second }: }",
+					"// alternative without defining type",
+					"function plus(numbers: [number, number]): number {     let a = numbers[0];     let b = numbers[0];     return a + b; }",
+					"let result = plus([3,5]);",
+				]}
+				links={[{ label: "React docs", url: "https://react.dev/learn/passing-props-to-a-component" }]}
+			/>
+			{/* <Content title="" body="" codeSnippets={["","",""]} links={[{ label: "", url: "" }]} /> */}
 		</div>
 	);
 }
