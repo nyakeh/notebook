18ce6a10484c04b6110f563c8599b053a03c9edf
Nyakeh Rogers <nyakeh@users.noreply.github.com>
2024-04-05 12:12:45 +0100

    Added code linting configuration using eslint. New chapter on unit testing code logic functions. Style change to rounded content box corners.

--- react-ts-guide/src/App.tsx
+++ react-ts-guide/src/App.tsx
@@ -11,9 +11,14 @@ function App() {
 				title="New project generation"
 				body="create-react-app is a command-line tool provided by the React team to bootstrap a new React project with minimal setup. It automates the process of setting up a modern React application, allowing developers to focus on writing code rather than configuring build tools and dependencies. The following command will generate files for a simple 'hello world' React project. Including a single UI webpage, stylesheet, and unit test."
 				code="npx create-react-app project-name --template typescript"
-				links={[{ label: 'Official docs', url: 'https://create-react-app.dev/docs/adding-typescript' }]}
+				links={[{ label: 'Create React App docs', url: 'https://create-react-app.dev/docs/adding-typescript' }]}
 			/>
-			<Content title="Second" body="" code="" links={[{ label: '', url: '' }]} />
+			<Content
+				title="Testing"
+				body="You can write unit tests using the Jest NPM library. Jest is a popular JavaScript testing framework that's commonly used for testing React applications. Using the beforeAll syntax 'beforeAll(() => {});' as a shared test setup function."
+				code="describe('Given a unit test method', () => { it('when in a test case', () => { const result = plus(4, 3); expect(result).toEqual(7); }); it('when a second case', () => { const result = plus(12, 4); expect(result).toEqual(16); }); });"
+				links={[{ label: 'Jest test setup docs', url: 'https://jestjs.io/docs/setup-teardown' }]}
+			/>
 			<Content title="Third" body="" code="" links={[{ label: '', url: '' }]} />
 		</div>
 	);
